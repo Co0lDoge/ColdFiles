d851fc47958569c1bea11ec2470a154abd576c3a
CoolDoge <thedarkguard60@gmail.com>
2024-07-31 19:31:23 +0500

    Add animation when file content grid changes size

--- app/src/main/java/com/example/coldfiles/ui/storage/StorageScreen.kt
+++ app/src/main/java/com/example/coldfiles/ui/storage/StorageScreen.kt
@@ -3,6 +3,9 @@ package com.example.coldfiles.ui.storage
 import android.app.Activity
 import android.content.Context
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -15,11 +18,13 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
@@ -31,12 +36,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.coldfiles.R
-import com.example.coldfiles.ui.theme.ColdFilesTheme
 import java.io.File
+import java.text.DateFormat
 
 
 @Composable
@@ -80,17 +84,30 @@ fun StorageScreenContent(
     context: Context,
     modifier: Modifier = Modifier,
 ) {
-    Card {
+    Card(
+        shape = RoundedCornerShape(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        ),
+        modifier = modifier
+            .padding(bottom = 8.dp)
+    ) {
         LazyVerticalGrid(
             columns = GridCells.Fixed(1),
             verticalArrangement = Arrangement.spacedBy(16.dp),
             horizontalArrangement = Arrangement.spacedBy(16.dp),
-            modifier = modifier.padding(16.dp)
+            modifier = Modifier
+                .padding(16.dp)
+                .animateContentSize(
+                    animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioNoBouncy,
+                        stiffness = Spring.StiffnessLow
+                    )
+                )
         ) {
             items(files) { file ->
-                FileCard(
-                    fileName = file.name,
-                    isFile = file.isFile,
+                FileItem(
+                    file = file,
                     modifier = Modifier.clickable {
                         // If file is clicked, open it
                         // Else move to selected directory
@@ -111,22 +128,21 @@ fun StorageScreenContent(
 }
 
 @Composable
-fun FileCard(
-    fileName: String,
-    isFile: Boolean,
+fun FileItem(
+    file: File,
     modifier: Modifier = Modifier
 ) {
     Row(modifier = modifier) {
         Icon(
-            painter = if (isFile) painterResource(id = R.drawable.file)
-                      else painterResource(id = R.drawable.folder),
-            contentDescription = fileName,
+            painter = if (file.isFile) painterResource(id = R.drawable.file)
+            else painterResource(id = R.drawable.folder),
+            contentDescription = file.name,
             modifier = Modifier.size(32.dp)
         )
         Spacer(modifier = Modifier.padding(horizontal = 16.dp))
         Column {
-            Text(text = fileName)
-            Text("additional info")
+            Text(text = file.name)
+            Text(text = DateFormat.getDateInstance().format(file.lastModified()))
             Spacer(modifier = Modifier)
             HorizontalDivider()
         }
@@ -170,16 +186,16 @@ fun StorageTopBar() {
     )
 }
 
-@Composable
-@Preview(showBackground = true)
-fun FileCardPreview() {
-    ColdFilesTheme {
-        FileCard(
-            fileName = "File Name",
-            isFile = false
-        )
-    }
-}
+//@Composable
+//@Preview(showBackground = true)
+//fun FileCardPreview() {
+//    ColdFilesTheme {
+//        FileCard(
+//            fileName = "File Name",
+//            isFile = false
+//        )
+//    }
+//}
 
 //@Composable
 //@Preview(showBackground = true)
