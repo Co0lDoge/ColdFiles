e40ea5aa57d8f7ebd3736de16b243864b52c5238
CoolDoge <thedarkguard60@gmail.com>
2024-09-09 18:12:41 +0500

    Convert SearchFilter to enum class

--- app/src/main/java/com/example/coldfiles/ui/search/SearchFilter.kt
+++ app/src/main/java/com/example/coldfiles/ui/search/SearchFilter.kt
@@ -1,23 +1,8 @@
 package com.example.coldfiles.ui.search
 
-sealed interface SearchFilter {
-
-    // Value to display
-    val name: String
-
-    data object Image : SearchFilter {
-        override val name = "Image"
-    }
-
-    data object Audio : SearchFilter {
-        override val name = "Audio"
-    }
-
-    data object Document : SearchFilter {
-        override val name = "Document"
-    }
-
-    data object InstallationFile: SearchFilter {
-        override val name = "Installation File"
-    }
+enum class SearchFilter(name: String) {
+    Image("Image"),
+    Audio("Audio"),
+    Document("Document"),
+    InstallationFile("Installation File")
 }
